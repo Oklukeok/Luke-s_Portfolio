07b4619201e7b60e0c5ec6ecc339299f3ccdd432
Oklukeok <lucasabermudez@gmail.com>
2022-09-13 09:17:57 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('trigger').addEventListener('click', () => {
+  document.getElementById('menuMobile').style.display = 'block';
+});
+
+document.getElementById('menuClose').addEventListener('click', () => {
+  document.getElementById('menuMobile').style.display = 'none';
+});
+
+document.getElementById('menuOption1').addEventListener('click', () => {
+  document.getElementById('menuMobile').style.display = 'none';
+});
+
+document.getElementById('menuOption2').addEventListener('click', () => {
+  document.getElementById('menuMobile').style.display = 'none';
+});
+
+document.getElementById('menuOption3').addEventListener('click', () => {
+  document.getElementById('menuMobile').style.display = 'none';
+});
